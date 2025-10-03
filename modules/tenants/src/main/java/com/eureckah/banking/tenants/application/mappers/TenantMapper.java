a84a3f646382a4f0257369369cf11eb9854eedea
Alexander Vivas <falexvr@gmail.com>
2025-10-03 22:50:29 +0200

    feat(tenants): added tenant mapper

---
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/application/mappers/TenantMapper.java
@@ -0,0 +1,15 @@
+package com.eureckah.banking.tenants.application.mappers;
+
+import com.eureckah.banking.tenants.application.views.TenantView;
+import com.eureckah.banking.tenants.domain.Tenant;
+
+public final class TenantMapper {
+
+    private TenantMapper() {
+
+    }
+
+    public TenantView toView(Tenant tenant) {
+        return new TenantView(tenant.getId().id(), tenant.getName());
+    }
+}
