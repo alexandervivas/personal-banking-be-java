4a64f944171a4f471ca989e87bc153765d07ef3d
Alexander Vivas <falexvr@gmail.com>
2025-10-03 21:09:04 +0200

    feat(tenants): added default no args constructor for hibernate later on and default getters

--- modules/tenants/src/main/java/com/eureckah/banking/tenants/domain/Profile.java
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/domain/Profile.java
@@ -1,5 +1,11 @@
 package com.eureckah.banking.tenants.domain;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Profile {
 
     private ProfileId id;
