b2dcc6ba92132c8be273b6535a2ccbb7768e45ff
Alexander Vivas <falexvr@gmail.com>
2025-10-03 16:36:23 +0200

    feat(tenants): added starter class for the module

--- modules/tenants/src/main/java/com/eureckah/banking/tenants/TenantsApplication.java
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/TenantsApplication.java
@@ -1,4 +1,11 @@
 package com.eureckah.banking.tenants;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class TenantsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TenantsApplication.class, args);
+    }
 }
