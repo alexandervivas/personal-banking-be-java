5f449c3974b7d94504b6cd51a8d7b1e7d5ca31c4
Alexander Vivas <falexvr@gmail.com>
2025-10-03 16:49:00 +0200

    feat(tenants): created user model

---
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/domain/User.java
@@ -0,0 +1,12 @@
+package com.eureckah.banking.tenants.domain;
+
+import lombok.Getter;
+
+@Getter
+public class User {
+
+    private UserId id;
+    private String name;
+    private String email;
+
+}
---
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/domain/UserId.java
@@ -0,0 +1,6 @@
+package com.eureckah.banking.tenants.domain;
+
+import java.util.UUID;
+
+public record UserId(UUID id) {
+}
