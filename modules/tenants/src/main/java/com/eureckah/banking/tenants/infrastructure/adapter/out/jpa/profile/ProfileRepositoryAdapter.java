1a60ad5aa83898b95b1b158ce6bdcb87dcaecec1
Alexander Vivas <falexvr@gmail.com>
2025-10-15 12:17:00 +0200

    fix(tenants): jpa adapter transient reference issue

--- modules/tenants/src/main/java/com/eureckah/banking/tenants/infrastructure/adapter/out/jpa/profile/ProfileRepositoryAdapter.java
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/infrastructure/adapter/out/jpa/profile/ProfileRepositoryAdapter.java
@@ -2,6 +2,10 @@ package com.eureckah.banking.tenants.infrastructure.adapter.out.jpa.profile;
 
 import com.eureckah.banking.tenants.application.port.out.storage.ProfileRepository;
 import com.eureckah.banking.tenants.domain.model.Profile;
+import com.eureckah.banking.tenants.infrastructure.adapter.out.jpa.tenant.TenantJpaEntity;
+import com.eureckah.banking.tenants.infrastructure.adapter.out.jpa.user.UserJpaEntity;
+
+import jakarta.persistence.EntityManager;
 
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Repository;
@@ -13,14 +17,27 @@ import java.util.UUID;
 public class ProfileRepositoryAdapter implements ProfileRepository {
 
     private final ProfileJpaRepository jpa;
+    private final EntityManager em;
 
-    public ProfileRepositoryAdapter(ProfileJpaRepository jpa) {
+    public ProfileRepositoryAdapter(ProfileJpaRepository jpa, EntityManager em) {
         this.jpa = jpa;
+        this.em = em;
     }
 
     @Override
     public UUID save(Profile profile) {
-        var entity = ProfileJpaMapper.toEntity(profile);
+        var tenantRef = em.getReference(TenantJpaEntity.class, profile.getTenant().getId());
+        var userId = profile.getUser().getId();
+        var userRef = em.getReference(UserJpaEntity.class, userId);
+
+        var entity =
+                ProfileJpaEntity.builder()
+                        .id(profile.getId())
+                        .tenant(tenantRef)
+                        .user(userRef)
+                        .role(profile.getRole())
+                        .build();
+
         return jpa.save(entity).getId();
     }
 }
