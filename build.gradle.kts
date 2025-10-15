796d15b4db9436e9dcbfe83ff5c145d991c821c4
Alexander Vivas <falexvr@gmail.com>
2025-10-15 13:36:14 +0200

    fix(build): disable spring boot plugin in root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 plugins {
-	java
-    id("org.springframework.boot") version "3.5.6"
-    id("io.spring.dependency-management") version "1.1.7"
-    id("com.google.protobuf") version "0.9.4"
+    java
+    id("org.springframework.boot") version "3.5.6" apply false
+    id("io.spring.dependency-management") version "1.1.7" apply false
+    id("com.google.protobuf") version "0.9.4" apply false
 }
 
 group = "com.eureckah.banking"
