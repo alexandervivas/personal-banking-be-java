7c0c932d72ce45ba02a6539ffb8a5aeb25777c0c
Alexander Vivas <falexvr@gmail.com>
2025-10-19 00:34:41 +0200

    fix(idempotency): also release idempotency keys on errors caused by bad arguments

--- modules/tenants/src/main/java/com/eureckah/banking/tenants/infrastructure/adapter/in/grpc/idempotency/AbstractIdempotentGrpcCreateHandler.java
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/infrastructure/adapter/in/grpc/idempotency/AbstractIdempotentGrpcCreateHandler.java
@@ -117,6 +117,7 @@ public abstract class AbstractIdempotentGrpcCreateHandler<Req, Resp> {
             // 6) Send success
             sendSuccess(createdId.get(), responseObserver);
         } catch (IllegalArgumentException ex) {
+            releaseIdempotencyKeyWithStatusCode(400);
             responseObserver.onError(
                     Status.INVALID_ARGUMENT
                             .withDescription("x-user-id must be provided")
