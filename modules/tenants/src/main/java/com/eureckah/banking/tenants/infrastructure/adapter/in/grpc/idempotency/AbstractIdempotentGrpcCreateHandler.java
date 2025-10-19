e332fe49a61376e2e0e559baf3ab844092b7da0b
Alexander Vivas <falexvr@gmail.com>
2025-10-19 04:49:45 +0200

    chore(tenants): remove extra catch since the error is already being handled elsewhere

--- modules/tenants/src/main/java/com/eureckah/banking/tenants/infrastructure/adapter/in/grpc/idempotency/AbstractIdempotentGrpcCreateHandler.java
+++ modules/tenants/src/main/java/com/eureckah/banking/tenants/infrastructure/adapter/in/grpc/idempotency/AbstractIdempotentGrpcCreateHandler.java
@@ -126,12 +126,6 @@ public abstract class AbstractIdempotentGrpcCreateHandler<Req, Resp> {
 
             // 6) Send success
             sendSuccess(createdId.get(), responseObserver);
-        } catch (IllegalArgumentException ex) {
-            releaseIdempotencyKeyWithStatusCode(400);
-            responseObserver.onError(
-                    Status.INVALID_ARGUMENT
-                            .withDescription("x-user-id must be provided")
-                            .asRuntimeException());
         } catch (InvalidCommandException ex) {
             releaseIdempotencyKeyWithStatusCode(422);
             responseObserver.onError(
